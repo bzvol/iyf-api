e383c450d2468ef7d9c263876d7d3fa65021e4d2
bzvol <zvolenszki.benjamin@gmail.com>
2024-02-03 19:02:00 +0100

    add aws secrets manager

--- src/IYFApi/Startup.cs
+++ src/IYFApi/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
 using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
+using IYFApi.Exceptions;
 using IYFApi.Repositories;
 using IYFApi.Repositories.Interfaces;
 using IYFApi.Services;
@@ -21,13 +25,13 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         AddDependencyInjection(services);
-        
+
         services.Configure<RouteOptions>(options =>
         {
             options.LowercaseUrls = true;
             options.LowercaseQueryStrings = true;
         });
-        
+
         services.AddControllers()
             .AddJsonOptions(options =>
             {
@@ -39,7 +43,7 @@ public class Startup
 
         FirebaseApp.Create(new AppOptions
         {
-            Credential = GoogleCredential.GetApplicationDefault(),
+            Credential = LoadFirebaseCredentials(),
             ProjectId = "iyfhu-caaf9"
         });
 
@@ -53,15 +57,34 @@ public class Startup
         services.AddSingleton<IEventRepository, EventRepository>();
         services.AddSingleton<IRegularEventRepository, RegularEventRepository>();
         services.AddSingleton<IVisitorRepository, VisitorRepository>();
-        
+
         services.AddSingleton<IDonationService, DonationService>();
         services.AddSingleton<IAccessManagementService, AccessManagementService>();
+    }
+
+    private static GoogleCredential LoadFirebaseCredentials()
+    {
+        if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+            return GoogleCredential.FromFile("~/iyfhu-firebase-adminsdk.json");
+        
+        var config = new AmazonSecretsManagerConfig
+            { RegionEndpoint = RegionEndpoint.GetBySystemName("eu-central-1") };
+        var client = new AmazonSecretsManagerClient(config);
+
+        var request = new GetSecretValueRequest
+            { SecretId = Environment.GetEnvironmentVariable("FIREBASE_CREDENTIALS_SECRET_NAME") };
+        var response = client.GetSecretValueAsync(request).Result;
+        var secret = response.SecretString;
+        
+        if (string.IsNullOrEmpty(secret)) throw new ConfigurationException("Firebase credentials not found");
+
+        return GoogleCredential.FromJson(secret);
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         ExceptionHandling.IsDevelopment = env.IsDevelopment();
-        
+
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
