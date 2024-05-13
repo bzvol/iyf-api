8d62c54b86bf7134f11090fe3a9255fb3399b810
bzvol <zvolenszki.benjamin@gmail.com>
2024-05-13 11:36:00 +0200

    only include past events in guest count

--- src/IYFApi/Services/InfoService.cs
+++ src/IYFApi/Services/InfoService.cs
@@ -10,10 +10,11 @@ public class InfoService(ApplicationDbContext context) : IInfoService
     {
         var posts = context.Posts.GroupBy(post => post.Status)
             .Select(postGroup => new CountGroup { Status = postGroup.Key, Count = postGroup.Count() });
+        var totalPosts = posts.Sum(p => p.Count);
 
         var postCountInfo = new PostCountInfo
         {
-            Total = posts.Sum(p => p.Count),
+            Total = totalPosts,
             Draft = GetCountForStatus(posts, Status.Draft),
             Published = GetCountForStatus(posts, Status.Published),
             Archived = GetCountForStatus(posts, Status.Archived)
@@ -21,30 +22,38 @@ public class InfoService(ApplicationDbContext context) : IInfoService
 
         var events = context.Events.GroupBy(ev => ev.Status)
             .Select(eventGroup => new CountGroup { Status = eventGroup.Key, Count = eventGroup.Count() });
+        var totalEvents = events.Sum(e => e.Count);
         var upcomingEvents = context.Events.Count(ev => ev.StartTime > DateTime.Now);
         var pastEvents = context.Events.Count(ev => ev.EndTime < DateTime.Now);
 
-        var guests = context.EventGuests.Count();
-        var uniqueGuests = context.EventGuests.ToList().Distinct(new EventGuest.EquivalencyComparer()).Count();
+        var guestsOfPastEvents = context.EventGuests.Join(
+            context.Events, eg => eg.EventId, ev => ev.Id,
+            (eg, ev) => new { Event = ev, Guest = eg })
+            .ToList()
+            .Where(j => j.Event.EndTime < DateTime.Now).Select(j => j.Guest)
+            .ToList();
+        var totalGuests = guestsOfPastEvents.Count;
+        var uniqueGuests = guestsOfPastEvents.Distinct(new EventGuest.EquivalencyComparer()).Count();
 
         var eventCountInfo = new EventCountInfo
         {
-            Total = events.Sum(e => e.Count),
+            Total = totalEvents,
             Upcoming = upcomingEvents,
             Past = pastEvents,
             Draft = GetCountForStatus(events, Status.Draft),
             Published = GetCountForStatus(events, Status.Published),
             Archived = GetCountForStatus(events, Status.Archived),
-            TotalGuests = guests,
+            TotalGuests = totalGuests,
             UniqueGuests = uniqueGuests
         };
 
         var regularEvents = context.RegularEvents.GroupBy(re => re.Status)
             .Select(revGroup => new CountGroup { Status = revGroup.Key, Count = revGroup.Count() });
+        var totalRegularEvents = regularEvents.Sum(re => re.Count);
 
         var regularEventCountInfo = new RegularEventCountInfo
         {
-            Total = regularEvents.Sum(re => re.Count),
+            Total = totalRegularEvents,
             Draft = GetCountForStatus(regularEvents, Status.Draft),
             Published = GetCountForStatus(regularEvents, Status.Published),
             Archived = GetCountForStatus(regularEvents, Status.Archived)
